77cc71195b96ab7c5ffc78d2722ebe8677225193
Deepak Dagar <dagardeepak88@gmail.com>
2018-08-04 23:24:48 +0530

    menu-button, typescript rotate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  showMenu : Boolean = false;
+
+  constructor(private el : ElementRef, private renderer:Renderer2){
+
+  }
+ 
 }
