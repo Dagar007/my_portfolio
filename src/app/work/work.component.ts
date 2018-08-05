81b97b7ae4161743ee489688b760fcbf62c943f3
Deepak Dagar <dagardeepak88@gmail.com>
2018-08-05 17:55:37 +0530

    work page done

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
   styleUrls: ['./work.component.scss']
 })
-export class WorkComponent implements OnInit {
+export class WorkComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor(private renderer: Renderer2) {
+    this.renderer.removeClass(document.body, 'bg-img');
+   }
 }
